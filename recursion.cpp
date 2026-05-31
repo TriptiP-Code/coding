ffe7673d5250b77c2703412d6e4f9653f9c724cd
Tripti Pandey <triptip033@gmail.com>
2026-05-31 04:06:45 +0000

    recursion

---
+++ recursion.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+using namespace std;
+
+
+// int cnt=1;
+// void print(int n){
+//     if(cnt==(n+1)) return;
+//     cout<<"tripti"<<endl;
+//     cnt++;  
+//     print(n);
+
+// }
+
+// int main(){
+//     int n;
+//     cout<<"enter value of n"<<endl;
+//     cin>>n;
+//  print(n);
+
+
+//  return 0;
+// }
+
+// ============================================================================
+
+// Print lineraly from 1 to n 
+
+// void f(int i,int n)
+// {
+//     if(i>n) return;
+
+//     cout<<i<<endl;
+//     f(i+1, n);
+    
+// }
+
+// int main()
+// {
+//     int n;
+//     cout<<"enter n"<<endl;
+//     cin>>n;
+//     f(1,n);
+
+//     return 0 ;
+// }
+
+
+// print in reverse order n to 1
+
+// void f(int n){
+//     if(n==0) return;
+//     cout<<n<<endl;
+//     n--;
+//     f(n);
+
+
+// }
+
+
+// int main()
+// {
+//     int n;
+//     cout<<"enter the n :" <<endl;
+//     cin>>n;
+//     f(n);
+//     return 0;
+// }
+
+// method backtracking for printing reverse n to 1 in recursion
+
+
+// void f(int i , int n){
+//     if(i>n) return;
+//     f(i+1,n);
+//     cout<<i<<endl;
+// }
+// int main()
+// {
+
+//     f(1,5);
+//     return 0;
+// }
+
+// sum of 1 to n using recursion
+
+
+
+
+
